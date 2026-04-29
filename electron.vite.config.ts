b0ff4afda0e5b7728c512baa5911cc57aa19325d
gianam65 <gianam65012001@gmail.com>
2026-04-29 09:32:46 +0700

    chore: add typescript and electron-vite config

---
+++ electron.vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: { outDir: 'out/main' },
+    resolve: {
+      alias: { '@shared': resolve(__dirname, 'src/shared') },
+    },
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: { outDir: 'out/preload' },
+    resolve: {
+      alias: { '@shared': resolve(__dirname, 'src/shared') },
+    },
+  },
+  renderer: {
+    plugins: [react()],
+    build: { outDir: 'out/renderer' },
+    root: '.',
+    resolve: {
+      alias: {
+        '@renderer': resolve(__dirname, 'src/renderer'),
+        '@shared': resolve(__dirname, 'src/shared'),
+      },
+    },
+  },
+});
